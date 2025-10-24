7844d92deef31e465b14de81fd9342705b99cb07
MichaelMachado <machado.michael60@gmail.com>
2025-10-24 17:08:17 -0300

    Login e Logout adicionado

--- cypress/e2e/WEB/auto_cenario_1.cy.js
+++ cypress/e2e/WEB/auto_cenario_1.cy.js
@@ -1,6 +1,4 @@
 describe('Cenario 01 - Cadastro', () => {
-
-
     it('ct 001 - Cadastro com sucesso', function () {
         cy.visit('https://luma-demo.scandipwa.com/')
         cy.get('#myAccount svg.UserIcon').click();
@@ -13,5 +11,20 @@ describe('Cenario 01 - Cadastro', () => {
         cy.get('[name="confirm_password"]').click();
         cy.get('#root button.MyAccountOverlay-SignUpButton').click();
 
-    })
-})
+    });
+
+    it('ct 008 - Login com sucesso ', function () {
+        cy.visit('https://luma-demo.scandipwa.com')
+        cy.get('#myAccount svg.UserIcon').click();
+        cy.get('[name="email"]').click();
+        cy.get('[name="password"]').click();
+        cy.get('#root div.MyAccountOverlay-SignInButton button.Button').click();
+    });
+
+    it('ct 009 - Logout com sucesso', function () {
+        cy.visit('https://luma-demo.scandipwa.com/')
+        cy.get('#myAccount svg.UserIcon').click();
+        cy.get('#root li:nth-child(8) button.MyAccountTabListItem-Button').click();
+
+    });
+});
